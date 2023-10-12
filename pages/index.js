211437b373ee0b5bfc442bbca64bbfafc0d40b32
yamini-choudhary <120270758+yamini-choudhary@users.noreply.github.com>
2023-10-12 11:20:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import CounterButton from '../components/CounterButton';
 
 export default function Home() {
     const [count, setCount] = useState(0);
-    const [dummyState, setDummyState] = useState(0); // New state for testing
+    const [dummyState, setDummyState] = useState(0);
 
     const incrementCount = useCallback(() => {
         setCount(prevCount => prevCount + 1);
@@ -12,7 +12,8 @@ export default function Home() {
     return (
         <div>
             <h1>Count: {count}</h1>
-            <CounterButton increment={incrementCount} /><br></br>
+            <CounterButton increment={incrementCount} />
+            <br></br>
             <button onClick={() => setDummyState(prev => prev + 1)}>Change Dummy State</button>
         </div>
     );
